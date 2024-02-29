6bf9c5db7a4265661df858eae4740dd933a0cbf8
FFX11 <110943620+FFX11@users.noreply.github.com>
2024-02-29 07:36:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8
 ./dotnet/dotnet --version
 ./dotnet8/dotnet workload install wasm-tools
 ./dotnet8/dotnet publish -c Release -o output
